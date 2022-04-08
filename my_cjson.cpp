0a33586c917b28436405cf3443698e61acf446fe
1637274818@qq.com <1637274818@qq.com>
2022-04-08 10:33:10 +0800

    update my_cjson.cpp

---
+++ my_cjson.cpp
@@ -0,0 +1,40 @@
+#ifndef MY_CJSON
+#define MY_CJSON
+
+namespace __MyCJSON__ {
+
+// JSON 对象
+template <class T1, class T2>
+class MycJSONObject {
+
+public:
+    // 键值的类型别名
+    typedef T1 key_type;
+    typedef T2 value_type;
+
+    // 构造函数
+
+private:
+    
+    // 成员数据
+    
+
+
+
+}；
+
+
+class MyCJSON {
+
+};
+
+
+
+
+
+}
+
+
+
+
+#endif
